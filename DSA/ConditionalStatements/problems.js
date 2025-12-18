61a849894b572842dbc60d2bdccd72f26551e3c7
Sameer-Shahzada <md.shahzada1606@gmail.com>
2025-12-18 23:20:46 +0530

    Conditional Statements problems

---
+++ DSA/ConditionalStatements/problems.js
@@ -0,0 +1,11 @@
+/* 
+    1- Accept two numbers and print the greatest number between them.
+    2- Accept an integer and check whether it is an even or odd number.
+    3- Accept name and age from the user. Check if the user is valid voter or not.
+    4- Accept three numbers and print the greatest among them.
+    5- Accept a year and check if it is a leap year or not.
+    6- Shop discount - description on graphic
+    7- Electricity bill - description on graphic
+    8- Counting number of days in a given month of a year.
+    
+*/
