2e8bb21480789db0054c9b4ba674684b0e184c73
Sameer-Shahzada <md.shahzada1606@gmail.com>
2025-12-22 00:42:10 +0530

    Loops Begins

---
+++ DSA/Loops/Introduction/printMsg.js
@@ -0,0 +1,21 @@
+// Accept an integer and print hello world n times 
+
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+const n = Number(prompt("Enter any number: "));
+
+// create a function 
+
+function printMessage(num) {
+    if (Number.isNaN(num) || num < 0) {
+        console.log("Invalid Input");
+        return;
+    }
+
+    for (let i = 1; i <= num; i++) {
+        console.log(`${i} Hello World`);
+    }
+}
+
+printMessage(n);
