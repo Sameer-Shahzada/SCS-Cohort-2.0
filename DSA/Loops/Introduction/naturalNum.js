3df14f663a49d3280bb80d02bfbf6979b15fcb9d
Sameer-Shahzada <md.shahzada1606@gmail.com>
2025-12-22 23:41:42 +0530

    Natural numbers

---
+++ DSA/Loops/Introduction/naturalNum.js
@@ -0,0 +1,17 @@
+// Write a program to print natural numbers from 1 to n
+
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+const num = Number(prompt('Enter any number: '));
+
+// create a function to print the natural number from 1 to n.
+function printNaturalNumbers(n) {
+    if(Number.isNaN(n) && n < 0) {
+        return 'Invalid input';
+    }
+    for(let i = 1; i <= n; i++) {
+        console.log(i)
+    }
+}
+printNaturalNumbers(num)
