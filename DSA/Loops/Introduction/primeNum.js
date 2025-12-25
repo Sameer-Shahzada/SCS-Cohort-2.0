445e03c8e9ca6d03a49042ab76a19761e025c9fd
Sameer-Shahzada <md.shahzada1606@gmail.com>
2025-12-25 22:59:45 +0530

    primeNumber

--- DSA/Loops/Introduction/primeNum.js
+++ DSA/Loops/Introduction/primeNum.js
@@ -1,7 +1,7 @@
 /* 
     Write a program to check the given number is prime or not. 
     Definition:
-        A number greater than 1 that has exactly two divisors: 1 and itself
+        A number greater than 1 that has exactly two divisors: 1 and itself.
 
 */
 
