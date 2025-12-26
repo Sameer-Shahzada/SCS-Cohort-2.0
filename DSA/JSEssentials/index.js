3f9a0160f37b1e216749549d833e686d0c3e6f7a
Sameer-Shahzada <md.shahzada1606@gmail.com>
2025-12-26 12:52:40 +0530

    factors & sumOfEvenOdd program

--- DSA/JSEssentials/index.js
+++ DSA/JSEssentials/index.js
@@ -265,3 +265,37 @@
 //   divisors.sort((a, b) => a - b).forEach((d) => console.log(d));
 // }
 // findFactors(num);
+
+// import promptSync from "prompt-sync";
+// const prompt = promptSync();
+
+// const startRange = Number(prompt("Enter range start point: "));
+// const endRange = Number(prompt("Enter range end point: "));
+
+// function sumEvenOddInRange(start, end) {
+//   if (!Number.isInteger(start) || !Number.isInteger(end)) {
+//     console.log("Invalid Input");
+//     return;
+//   }
+//   // swap if start is greater than end
+//   if (start > end) {
+//     // let temp = start;
+//     // start = end;
+//     // end = temp;
+
+//     [start, end] = [end, start];
+//   }
+//   let evenSum = 0;
+//   let oddSum = 0;
+
+//   for (let i = start; i <= end; i++) {
+//     if (i % 2 === 0) {
+//       evenSum += i;
+//     } else {
+//       oddSum += i;
+//     }
+//   }
+//   console.log("evenSum: ", evenSum);
+//   console.log("oddSum: ", oddSum);
+// }
+// sumEvenOddInRange(startRange, endRange);
