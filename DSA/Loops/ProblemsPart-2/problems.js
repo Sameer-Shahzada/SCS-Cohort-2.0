9b5977b739432aa1c3dd6e9f97ae3de90d718462
Sameer-Shahzada <md.shahzada1606@gmail.com>
2025-12-29 01:17:29 +0530

    Loop Problem Part-2 | Problem Set

---
+++ DSA/Loops/ProblemsPart-2/problems.js
@@ -0,0 +1,18 @@
+/* 
+1- Write a program to take two inputs a, b & find the value of a raised to the power of b.
+    Example: a = 2, b = 5
+    Output: 2^5 = 32
+
+2- break and continue (topic)
+    -> while loop
+
+3- sum of digits
+4- reverse a number
+5- strong number 
+6- Automorphic number
+7- Armstrong number
+8- Palindrome number
+9- Fibonacci series
+
+
+*/
