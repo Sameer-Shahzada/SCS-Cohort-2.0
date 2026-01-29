6d3cfc47843a75b880fc5ae20fdd2e129157adb8
Sameer-Shahzada <md.shahzada1606@gmail.com>
2026-01-29 17:18:03 +0530

    connection to database

--- Backend/Notes/server.js
+++ Backend/Notes/server.js
@@ -1,4 +1,11 @@
+/** Purpose of server.js file 
+ * - Start the server 
+ * - connect to database
+ */
+
+
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,6 +14,39 @@ const PORT = process.env.PORT || 3000;
 //     { id: 1, title: 'First note', description: 'This is the first note' },
 //     { id: 2, title: 'Second note', description: 'This is the second note' },
 // ];
+
+
+// create a function to connect with database 
+
+// create a function to connect with database 
+// function connectToDatabase() {
+//   // Database connection logic goes here 
+//   const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://mohdsameershahzada_db_user:vL9bNTxBfdyknme8@cluster0.3ekwfva.mongodb.net/notesDB";
+//   mongoose.connect(MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//     .then(() => {
+//       console.log("Connected to MongoDB database");
+//     })
+//     .catch((error) => {
+//       console.error("Error connecting to MongoDB database:", error);
+//     });
+// }
+
+function connectToDatabase() {
+  // Database connection logic goes here 
+  mongoose.connect("mongodb+srv://mohdsameershahzada_db_user:vL9bNTxBfdyknme8@cluster0.3ekwfva.mongodb.net/notesDB", {
+  })
+    .then(() => {
+      console.log("Connected to MongoDB database");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB database:", error);
+    });
+}
+
+connectToDatabase()
 
 const notes = [];
 
@@ -61,3 +101,14 @@ app.delete("/notes/:id", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+
+/**
+ * Credentials for Mongo DB Altas - SCS Cohort 2.0 project 
+ * Username: mohdsameershahzada_db_user
+ * Password: vL9bNTxBfdyknme8
+ * 
+ * 
+ */
