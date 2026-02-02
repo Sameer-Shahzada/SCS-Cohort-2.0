7d24c37c26c9f00167c46ba279cb15f530eb05ad
Sameer-Shahzada <md.shahzada1606@gmail.com>
2026-02-02 12:04:38 +0530

    MouseFollower mini project

---
+++ CSS/Day-92_MouseFollower/script.js
@@ -0,0 +1,18 @@
+const mouseFollower = document.querySelector('.mouse-follower');
+
+let x = 0;
+let y = 0;
+
+addEventListener('mousemove', (e) => {
+    const { clientX, clientY } = e;
+    x = clientX;
+    y = clientY;
+    // animate();
+
+})
+
+function animate() {
+    mouseFollower.style.transform = `translate(${x}px, ${y}px)`;
+    requestAnimationFrame(animate);
+}
+animate();
