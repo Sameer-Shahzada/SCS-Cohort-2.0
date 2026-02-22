d0fae37a105c3ea69934ed68ace79103cc1c0f59
Sameer-Shahzada <md.shahzada1606@gmail.com>
2026-02-23 01:35:16 +0530

    SCS Cohort DSA problem sheet added

---
+++ DSA/SCS_DSA/SCS_DSA_Questions_Sheet/dsa_coding_problems.js
@@ -0,0 +1,242 @@
+/*
+// ---------------------------------- | Math Questions | ------------------------------------ //
+
+1. Accept two numbers from user and swap their values (Part 2 - Swap without using third variable)
+2. Find surface area of sphere
+3. Find circumference and area of circle
+
+// ---------------------------------- | Conditionals Statements | ------------------------------------ //
+4. Accept name and age from the user. Check if the user is a valid
+voter or not. (Valid - Hello Shery, You are a valid voter. Invalid - Sorry
+Shery, you can't cast the vote. Part 2 - Print after how many years
+the user will be eligible)
+
+5. Accept a day number between 1-7 and print the corresponding day name.
+6. Accept three numbers and print the greatest among them
+7. Movie rating
+8. Profile based program
+9. Accept an English alphabet from user and check if it is a consonant
+or a vowel.
+
+// ---------------------------------- | Loops | ------------------------------------ //
+
+10. Factorial of a number
+11. Write a program to take two inputs a, b & find the value of a raised
+to the power of b. (Ex - a = 2, b = 5, OP - 2^5 = 32)
+
+12. Harshad number
+13. Perfect square
+14. Abundant number
+15. Find Number of integers which has exactly x divisors
+16. Program for Finding out the Prime Factors of a number
+
+17. A neon number is a number where the sum of digits of the square
+of the number is equal to the number. (Ex: Input : 9, Output : Given
+number 9 is Neon number)
+
+18. Armstrong number
+19. Perfect number
+20. Friendly pairs
+21. Permutations in which n people can occupy r seats in a classroom
+22. Maximum number of handshakes
+23. For a given positive integer N, find the value of F2 + F4 + F6 +.........+
+F2n till N number, where Fi indicates the i’th Fibonacci number. (Ex:
+n = 4, Output: 33)
+
+// ---------------------------------- | Nested Loops | ------------------------------------ //
+
+24. Accept a number and check if it is a strong number or not (Sum of factorial of each digit, Ex: 145 = 1! + 4! + 5! = 145)
+25. Right Triangle - Number Pattern
+26. Right Triangle - Alphabet Pattern
+
+------ | Methods |------
+
+27. Strong number using methods.
+28. Special number using methods.
+
+
+------ | Array |------
+
+29. Concatenation of array
+30. Build Array from Permutation
+
+31. Given a sorted array of distinct elements, find the summation of
+absolute differences of all pairs in the given array. (Ex: arr[] = {1, 2, 3, 4},
+Output: 10)
+
+32. You are given two arrays of equal length: An integer array A[] and a
+character array B[], where each character is one of 'a', 'b', or 'c'. Find the
+minimum value of the expression: min(sum of values linked with 'a' +
+sum of values linked with 'b', sum of values linked with 'c').
+
+33. Given an array of integers, update every element with the
+multiplication of previous and next elements with the following
+exceptions: a) The First element is replaced by the multiplication of the
+first and second. b) The last element is replaced by multiplication of
+the last and second last. (Ex: Input : arr[] = {2, 3, 4, 5, 6}, Output : arr[] = {6,
+8, 15, 24, 30})
+
+34. We are given an array of Integers. We have to perform the following
+operation on the array until it is fully exhausted: Select the max number
+in the array and delete that number including all the numbers to its
+right side in the array. Repeat this step for the left elements of the
+array i.e select the maximum element in the left elements and delete it
+including all numbers to its right. Our task is to simulate the above
+procedure and return the number of steps that will be taken until the
+first element (index 0) of the array is also deleted and the array is
+exhausted. (Ex: Array = [2, 3, 5, 4, 1], Output : Steps Taken: 3)
+
+35. Given an array of even size, task is to find minimum value that can be
+added to an element so that the array becomes balanced. An array is
+balanced if the sum of the left half of the array elements is equal to the
+sum of the right half. (Ex: Input : 1 2 1 2 1 3, Output : 2)
+
+36. Sort First half in Ascending and Second half in descending order in an
+array
+
+37. Find sum of minimum absolute difference of the given array
+
+--------------- | Multi-D Array | ----------------- 
+38. Valid Sudoku
+
+------------------ | String |---------------------
+
+39. Count Asterisks
+40. Percentage of Letter in String
+41. Check if All A's Appears Before All B's
+42. Strong Password Checker II
+43. Check distances Between same letters
+44. Greatest English Letter in Upper and Lower Case
+45. Rearrange Characters to Make Target String
+46. Largest 3-Same-Digit Number in String
+47. Remove Digit From Number to Maximize Result
+48. Check if Number Has Equal Digit Count and Digit Value
+49. Find Resultant Array After Removing Anagrams
+50. Calculate Digit Sum of a String
+51. Check Whether Two Strings are Almost Equivalent
+52. Check if Numbers Are Ascending in a Sentence
+53. Reverse Prefix of Word
+54. Hashing:
+55. Decode the Message
+56. Distribute Candies
+57. Find the Difference
+58. Count Vowel Substrings of a String
+59. Kth Distinct String in an Array
+60. Word Pattern
+61. Set Mismatch
+62. Unique Morse Code Words
+63. Most Common Word
+
+------------------ | Advance Math Problem |-----------
+
+64. Median (--Sorted data, --In case of odd count - pick the middle
+element, --In case of even count - pick the two middle elements and
+calc their mean)
+
+65. The kth Factor of n
+
+--------------------- | Advance Array | ------------------
+
+66. Replace Elements with Greatest Element on Right Side
+67. Squares of a Sorted Array
+68. Move Zeroes
+69. Sort Array By Parity
+70. Running Sum of 1d Array
+71. Find Pivot Index
+72. Product of Array Except Self
+73. Best Time to Buy and Sell Stock II
+
+--------------------- | Cyclic Sort |-----------
+74.Set Mismatch
+75. Find All Duplicates in an Array
+76. Find the Duplicate Number
+
+------------------ | Binary Search |--------------
+
+77. Binary Search on descending sorted array
+78. Guess Number Higher or Lower
+79. Ceiling of a number in a sorted array
+80. Floor of a number in a sorted array
+81. Find Smallest Letter Greater Than Target
+82. Valid Perfect Square
+83. First Bad Version
+84. Search in an Infinite sorted array
+85. Search a 2D Matrix
+86. Capacity To Ship Packages Within D Days
+87. Minimum Number of Days to Make m Bouquets
+88. Find the Smallest Divisor Given a Threshold
+89. Split Array Largest Sum
+90. Aggressive cows
+
+--------------- | Advance Hashing | ---------------
+91. Insert Delete GetRandom O(1)
+
+--------------- | Linked List | ---------------
+
+92. Design Linked List
+93. Linked List Cycle II
+94. Convert Binary Number in a Linked List to Integer
+95. Design HashSet
+96. Design HashMap
+97. Remove Duplicates from Sorted List II
+98. wap Nodes in Pairs
+99. Remove Nth Node From End of List
+100. Add Two Numbers
+
+------------- | Queue and Stack |--------------------
+101. Smaller on Left
+102. Next Greater Element I
+103. Smallest number on left
+104. Backspace String Compare (Try it in O(1) space - Two pointers)
+105. Online Stock Span
+106. Min Stack (Two Stack / Linked List with min var / Single stack)
+107. Largest Rectangle in Histogram
+108. Maximal Rectangle
+
+------------------- | Backtracking |-------------
+
+109. Permutations II
+110. Combination Sum II
+111. Combination Sum III
+112. N-queen ||
+113. Rat in a maze
+114.Permutation Sequence
+
+----------- | Binary Tree | ----------------
+
+115. Binary Tree Level Order Traversal II
+116. Populating Next Right Pointers in Each Node II
+117. Right View of Binary Tree
+118. Bottom View of Binary Tree
+119. Vertical Order Traversal
+120. Sum of the Longest Bloodline of a Tree
+121. Construct Binary Tree from Inorder and Postorder Traversal
+122. Burning Tree
+
+------------- | Binary Search Tree |-------------
+
+123. Predecessor and Successor
+124. Lowest Common Ancestor of a Binary Search Tree
+125. Two Sum IV - Input is a BST
+126. Convert Sorted Array to Binary Search Tree
+127. Convert Sorted List to Binary Search Tree
+128. Recover Binary Search Tree
+129. Subtree of Another Tree
+
+—Binary heap
+130. Sort Characters By Frequency
+
+—sliding window
+131. Pick Toy
+
+—Graph
+132. Bridges in a graph using Tarjan’s algorithm
+133. Articulation point
+134. Kosaraju's Algorithm
+
+—DP
+135. Edit Distance
+136. Longest Increasing Subsequence
+137. Count Square Sub-matrixes with All Ones
+138. Scramble string
+*/
