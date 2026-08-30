505f70d535dbcc1006b62cabba9c204cf5caa170
Sameer-Shahzada <md.shahzada1606@gmail.com>
2026-08-30 23:07:46 +0530

    SCS_YT_DSA Basic lecture

---
+++ DSA/SCS_DSA/SCS_Yt_DSA/Basics/sumOfTwoInteger.js
@@ -0,0 +1,6 @@
+// Write a program to find the sum of two integers 
+let num1 = 6;
+let num2 = 10; 
+
+let sum =  num1 + num2; 
+console.log(sum);   // 16
---
+++ DSA/SCS_DSA/SCS_Yt_DSA/Basics/swapping.js
@@ -0,0 +1,10 @@
+// Write a program to swap two numbers 
+
+let a = 10; 
+let b = 20; 
+
+a = a + b;  // 30
+b = a - b;  // 10 
+a = a - b;  // 20 
+
+console.log(a, b)
