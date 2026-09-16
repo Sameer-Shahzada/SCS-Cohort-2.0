b385611dd92ebad0f8c8140dab87db7cbe712979
Sameer-Shahzada <md.shahzada1606@gmail.com>
2026-09-16 17:52:20 +0530

    modification

--- 1-SCS_Yt_JS/Basic/Loops/loops.js
+++ 1-SCS_Yt_JS/Basic/Loops/loops.js
@@ -65,7 +65,7 @@ let count = 0;
 
 for(let i = 1; i <= 100; i++) {
     if(i % 3 == 0 && i % 5 == 0) {
-        console.log(i);
+
         count ++;
     }                                              
 }
